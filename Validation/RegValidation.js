fd507435334e733b5c7da07138e81c5934e7625e
kokaneva <kokane.vina@gmail.com>
2023-03-14 17:13:13 +0530

    Validation

---
+++ Validation/RegValidation.js
@@ -0,0 +1,95 @@
+
+let fnameNode=document.getElementById('fname');
+let err1Node=document.getElementById('error1');
+
+let ageNode=document.getElementById('age');
+let err2Node=document.getElementById('error2');
+
+let mailNode=document.getElementById('emailid');
+let err3Node=document.getElementById('error3');
+
+let passNode=document.getElementById('password');
+let err4Node=document.getElementById('error4');
+
+let cpassNode=document.getElementById('confirmpass');
+let err5Node=document.getElementById('error5');
+
+let emptyField="This field is required";
+
+let namePattern= new RegExp("^[A-Za-z]*$");
+let passwordPattern=new RegExp('^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,12}$');
+
+function validate1(){
+    err1Node.innerHTML="";
+    let firstName=fnameNode.value;
+    if(firstName==''){
+        err1Node.innerHTML=emptyField;
+    }
+    else if(!namePattern.test(firstName)){
+        err1Node.innerHTML="Name should have only characters";
+    }
+    else{
+
+    }
+}
+
+function validate2(){
+    err2Node.innerHTML="";
+    let age=ageNode.value;
+    if(age==''){
+        err2Node.innerHTML=emptyField;
+    }
+    else if(age<20 || age>50){
+        err2Node.innerHTML="Age should be between 20 to 50";
+    }
+    else{
+
+    }
+}
+
+
+function validate3(){
+    err3Node.innerHTML="";
+    let emailId=mailNode.value;
+    if(emailId==''){
+        err3Node.innerHTML=emptyField;
+    }
+    else if(!emailId.includes('@') || emailId.endsWith('@')){
+        err3Node.innerHTML="Please enter valid email id";
+    }
+    else{
+
+    }
+}
+
+
+function validate4(){
+    err4Node.innerHTML="";
+    let pass=passNode.value;
+    //console.log(passwordPattern.test(pass));
+    if(pass==''){
+        err4Node.innerHTML=emptyField;
+    }
+    else if(!passwordPattern.test(pass)){
+        err4Node.innerHTML="Password should have atleast one 1. small letter, 2. big letter, 3. digit, 4. symbol(). and it should be 6 to 12 chars long";
+    }
+    else{
+
+    }
+}
+
+function validate5(){
+    err5Node.innerHTML="";
+    let confirmPass=cpassNode.value;
+    let pass=passNode.value;
+    //console.log(passwordPattern.test(pass));
+    if(confirmPass==''){
+        err5Node.innerHTML=emptyField;
+    }
+    else if(confirmPass!=pass){
+        err5Node.innerHTML="Password should match";
+    }
+    else{
+
+    }
+}
